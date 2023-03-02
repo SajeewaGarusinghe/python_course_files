58e01bc9e55046a40f4045dadabc21cf7cb21ca4
sajeewa garusinghe <sajeeva616@gmail.com>
2023-03-02 18:38:21 +0530

    new course

---
+++ hello.py
@@ -0,0 +1,23 @@
+# # name = input('what is you name ?')
+# # print('hellow '+name)
+# print(22//3)
+# print(int('0b101',2))
+# iq=23
+# rt=34
+#
+# PI=3.14
+# print(type(PI))
+# a,b,c=1,2,3
+# print(a,b,c)
+# r='''s
+# sssssssss
+# sssssss
+# ssss'''
+# print(r)
+#
+#
+name='sajeewa'#dhgbbbn
+age=28#dhgbbbn
+print(f'hi my name is {name} and ahe is {age}')#formatted string#dhgbbbn
+
+
